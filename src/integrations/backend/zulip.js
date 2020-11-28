7e5eb08b1bc1674a637ece4ed3a386f14bd5e33b
Felix <felixletsplay@outlook.de>
2020-11-28 21:40:57 +0100

    :sparkles: Show new Zulip messages

--- src/integrations/backend/zulip.js
+++ src/integrations/backend/zulip.js
@@ -30,7 +30,34 @@ export class Zulip extends Integration {
         }
     }
 
+    getLoginData() {
+        return {
+            site: localStorage.getItem('int_zulip_site'),
+            token: localStorage.getItem('int_zulip_token'),
+            email: localStorage.getItem('int_zulip_email')
+        }
+    }
+
     isConnected() {
         return localStorage.getItem('int_zulip_site') && localStorage.getItem('int_zulip_token') && localStorage.getItem('int_zulip_email');
+    }
+
+    async getAllData() {
+        let data = await this.getUnreadMessages();
+        return {'unread': data};
+    }
+
+    getUnreadMessages() {
+        return new Promise((resolve, reject) => {
+            let loginData = this.getLoginData();
+            let req = new XMLHttpRequest();
+            req.onloadend = () => {
+                let resp = JSON.parse(req.responseText);
+                resolve(resp);
+            }
+            req.open('GET', loginData.site + '/api/v1/messages?anchor=first_unread&num_before=0&num_after=1000')
+            req.setRequestHeader("Authorization", "Basic " + btoa(loginData.email + ':' + loginData.token))
+            req.send()
+        });
     }
 }
--- src/integrations/frontend/zulip.js
+++ src/integrations/frontend/zulip.js
@@ -4,4 +4,20 @@ document.getElementById('addZulipButton').onclick = () => zulip.login();
 
 if (zulip.isConnected()) {
     document.getElementById('addZulipButton').hidden = true; //TODO disable instead of hide
+    $("#zulipFrame").load("integrations/frames/Zulip.html");
+    zulip.getAllData().then((data) => {
+        let siteURL = zulip.getLoginData().site;
+        $(".zulip_url").text(siteURL);
+        for (let message in data.unread.messages) {
+            if (!data.unread.messages.hasOwnProperty(message)) continue;
+            message = data.unread.messages[message];
+            let messageElement = document.createElement('li');
+            message.content = message.content.replace('href="/user', 'href="' + siteURL + '/user')
+            message.content = message.content.replace('src="/user', 'src="' + siteURL + '/user')
+            let recipent = typeof message.display_recipient === 'string' ? message.display_recipient + ' (' + message.subject + ')' : message.display_recipient[0]['full_name'];
+            messageElement.innerHTML = '<b>' + message.sender_full_name + ' -> ' + recipent + ':</b> ' + message.content;
+            document.getElementById('zulip_messages').appendChild(messageElement);
+        }
+        document.getElementById('zulipFrame').hidden = false;
+    })
 }
