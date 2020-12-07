a89fc2997e62ed4e8ffb3c7f693ae43e4770a4d7
Felix <felixletsplay@outlook.de>
2020-12-07 10:43:33 +0100

    :sparkles: Add multi account per integration support

--- src/integrations/frontend/moodle.js
+++ src/integrations/frontend/moodle.js
@@ -4,41 +4,42 @@ import { setUserIsLoggedIn,showIntegration } from "./common.js";
 document.getElementById('addMoodleButton').onclick = () => Moodle.login();
 
 if (Moodle.isConnected()) {
-    let moodle = new Moodle(Moodle.getAllLogins()[0]);
-    document.getElementById('addMoodleButton').classList.add('disabled');
     setUserIsLoggedIn();
-    moodle.getAllData().then((data) => {
-        showIntegration('Moodle',
-            {
-                'moodle_siteName': data.name,
-                'moodle_firstName': data.firstName,
-                'moodle_unread': data.unread
-            },
-            (frame) => {
-                frame.getElementsByClassName('moodle_messages_link')[0].href = moodle.loginCredentials.site + '/message/index.php';
-                for (let event in data.timeline) {
-                    if (!data.timeline.hasOwnProperty(event)) continue;
-                    event = data.timeline[event];
-                    let rowElement = document.createElement('tr');
-                    let titleElement = document.createElement('td');
-                    titleElement.innerText = event.name;
-                    let timeElement = document.createElement('td');
-                    timeElement.innerText = new Date(event.timestart * 1000).toLocaleString();
-                    rowElement.appendChild(titleElement);
-                    rowElement.appendChild(timeElement);
-                    frame.getElementsByClassName('moodle_abgaben')[0].appendChild(rowElement);
-                }
-                for (let forum in data.news) {
-                    if (!data.news.hasOwnProperty(forum)) continue;
-                    forum = data.news[forum];
-                    for (let forumPost in forum) {
-                        if (!forum.hasOwnProperty(forumPost)) continue;
-                        forumPost = forum[forumPost];
-                        let element = document.createElement('div');
-                        element.innerHTML = '<h6>' + forumPost.userfullname + ' <i>(' + forumPost.subject + ")</i> </h6>" + forumPost.message;
-                        frame.getElementsByClassName('moodle_news')[0].appendChild(element);
+    Moodle.getAllLogins().forEach((id) => {
+        let moodle = new Moodle(id);
+        moodle.getAllData().then((data) => {
+            showIntegration('Moodle',
+                {
+                    'moodle_siteName': data.name,
+                    'moodle_firstName': data.firstName,
+                    'moodle_unread': data.unread
+                },
+                (frame) => {
+                    frame.getElementsByClassName('moodle_messages_link')[0].href = moodle.loginCredentials.site + '/message/index.php';
+                    for (let event in data.timeline) {
+                        if (!data.timeline.hasOwnProperty(event)) continue;
+                        event = data.timeline[event];
+                        let rowElement = document.createElement('tr');
+                        let titleElement = document.createElement('td');
+                        titleElement.innerText = event.name;
+                        let timeElement = document.createElement('td');
+                        timeElement.innerText = new Date(event.timestart * 1000).toLocaleString();
+                        rowElement.appendChild(titleElement);
+                        rowElement.appendChild(timeElement);
+                        frame.getElementsByClassName('moodle_abgaben')[0].appendChild(rowElement);
                     }
-                }
-            });
-    });
+                    for (let forum in data.news) {
+                        if (!data.news.hasOwnProperty(forum)) continue;
+                        forum = data.news[forum];
+                        for (let forumPost in forum) {
+                            if (!forum.hasOwnProperty(forumPost)) continue;
+                            forumPost = forum[forumPost];
+                            let element = document.createElement('div');
+                            element.innerHTML = '<h6>' + forumPost.userfullname + ' <i>(' + forumPost.subject + ")</i> </h6>" + forumPost.message;
+                            frame.getElementsByClassName('moodle_news')[0].appendChild(element);
+                        }
+                    }
+                });
+        });
+    })
 }
--- src/integrations/frontend/zulip.js
+++ src/integrations/frontend/zulip.js
@@ -4,28 +4,29 @@ import { setUserIsLoggedIn, showIntegration } from "./common.js";
 document.getElementById('addZulipButton').onclick = () => Zulip.login();
 
 if (Zulip.isConnected()) {
-    let zulip = new Zulip(Zulip.getAllLogins()[0]);
-    document.getElementById('addZulipButton').classList.add('disabled');
     setUserIsLoggedIn();
-    zulip.getAllData().then((data) => {
-        let siteURL = zulip.loginCredentials.site;
-        showIntegration('Zulip', {
-            'zulip_url': siteURL.replace('http://', '').replace('https://', ''),
-            'zulip_email': zulip.loginCredentials.email,
-            'zulip_unread_count': data.unread.length
-        }, (frame) => {
-            frame.getElementsByClassName('zulip_messages_link')[0].href = siteURL;
-            for (let message in data.unread) {
-                if (!data.unread.hasOwnProperty(message)) continue;
-                message = data.unread[message];
-                let messageElement = document.createElement('li');
-                message.content = message.content.replace('href="/user', 'href="' + siteURL + '/user')
-                message.content = message.content.replace('src="/user', 'src="' + siteURL + '/user')
-                let recipent = typeof message.display_recipient === 'string' ? message.display_recipient + ' (' + message.subject + ')' : message.display_recipient[0]['full_name'];
-                messageElement.innerHTML = '<b>' + message.sender_full_name + ' -> ' + recipent + ':</b> ' + message.content;
-                frame.getElementsByClassName('zulip_messages')[0].appendChild(messageElement);
-            }
-        });
+    Zulip.getAllLogins().forEach((id) => {
+        let zulip = new Zulip(id);
+        zulip.getAllData().then((data) => {
+            let siteURL = zulip.loginCredentials.site;
+            showIntegration('Zulip', {
+                'zulip_url': siteURL.replace('http://', '').replace('https://', ''),
+                'zulip_email': zulip.loginCredentials.email,
+                'zulip_unread_count': data.unread.length
+            }, (frame) => {
+                frame.getElementsByClassName('zulip_messages_link')[0].href = siteURL;
+                for (let message in data.unread) {
+                    if (!data.unread.hasOwnProperty(message)) continue;
+                    message = data.unread[message];
+                    let messageElement = document.createElement('li');
+                    message.content = message.content.replace('href="/user', 'href="' + siteURL + '/user')
+                    message.content = message.content.replace('src="/user', 'src="' + siteURL + '/user')
+                    let recipent = typeof message.display_recipient === 'string' ? message.display_recipient + ' (' + message.subject + ')' : message.display_recipient[0]['full_name'];
+                    messageElement.innerHTML = '<b>' + message.sender_full_name + ' -> ' + recipent + ':</b> ' + message.content;
+                    frame.getElementsByClassName('zulip_messages')[0].appendChild(messageElement);
+                }
+            });
 
+        })
     })
 }
